8bb57436ab50451383779760c966c4106ff9927e
Johny Wills <jonathan_wills@icloud.com>
2021-05-22 16:41:44 +0900

    Fixed IssueEntity with correct types and relations

--- database/entities/IssueEntity.ts
+++ database/entities/IssueEntity.ts
@@ -1,76 +1,70 @@
-import { BeforeInsert, BeforeUpdate, Column, Entity, JoinTable, ManyToMany, ManyToOne, OneToMany, PrimaryGeneratedColumn, RelationId } from 'typeorm';
-import { IssuePriority, IssueType } from '../../constants/issues';
+import { Column, Entity, JoinTable, ManyToMany, ManyToOne, OneToMany, PrimaryGeneratedColumn, RelationId } from "typeorm";
+import { IssuePriority, IssueType } from "../../constants/issues";
 
+import { ProjectEntity } from "./ProjectEntity";
+import { UserEntity } from "./UserEntity";
+import { CommentEntity } from "./CommentEntity";
 @Entity()
 export class IssueEntity {
-    @PrimaryGeneratedColumn()
-    id!: number;
+	@PrimaryGeneratedColumn("uuid")
+	id!: number;
 
-    @Column('varchar')
-    title!: string;
+	@Column("varchar")
+	title!: string;
 
-    @Column('varchar')
-    type!: IssueType;
+	@Column("varchar")
+	type!: IssueType;
 
-    @Column('varchar')
-    status!: string;
+	@Column("varchar")
+	status!: string;
 
-    // Make issue priority enum type of
-    // Lowest, Low, Medium, High, Highest
-    @Column('varchar')
-    priority!: IssuePriority;
+	// Make issue priority enum type of
+	// Lowest, Low, Medium, High, Highest
+	@Column("varchar")
+	priority!: IssuePriority;
 
-    @Column('text', { nullable:true })
-    description!: string | null;
+	@Column("text", { nullable: true })
+	description!: string | null;
 
-    @Column('integer', { nullable: true })
-    timeEstimate!: number | null;
+	@Column("integer", { nullable: true })
+	timeEstimate!: number | null;
 
-    @Column('integer', { nullable: true })
-    timeSpent!: number | null;
+	@Column("integer", { nullable: true })
+	timeSpent!: number | null;
 
-    @Column('integer', { nullable: true })
-    timeRemaining!: number | null;
+	@Column("integer", { nullable: true })
+	timeRemaining!: number | null;
 
-    @Column({ type: 'timestamp' })
-    createdAt!: Date;
+	@Column({ type: "timestamp" })
+	createdAt!: Date;
 
-    @Column({ type: 'timestamp' })
-    updatedAt!: Date;
+	@Column({ type: "timestamp" })
+	updatedAt!: Date;
 
-    @Column('integer')
-    reporterId!: number;
+	@Column("integer")
+	reporterId!: number;
 
-    @ManyToOne(
-        () => Project,
-        project => project.issues,
-    )
-    project: Project;
+	@ManyToOne(() => ProjectEntity, (project) => project.issues)
+	project!: ProjectEntity;
 
-    @Column('integer')
-    projectId!: number;
+	@Column("integer")
+	projectId!: number;
 
-    @OneToMany(
-        () => Comment,
-        comment => comment.issue,
-    )
-    comments!: Comment[];
+	@OneToMany(() => CommentEntity, (comment) => comment.issue)
+	comments!: Comment[];
 
-    @ManyToMany(
-        () => userInfo,
-        user => user.issues,
-    )
-    @JoinTable()
-    users!: User[];
+	@ManyToMany(() => UserEntity, (user) => user.issues)
+	@JoinTable()
+	users!: UserEntity[];
 
-    @RelationId((issue: IssueEntity) => issue.users)
-    userIds!: number[];
+	@RelationId((issue: IssueEntity) => issue.users)
+	userIds!: number[];
 
-    // @BeforeInsert()
-    // @BeforeUpdate()
-    // setDescriptionText = (): void => {
-    //     if(this.description) {
-    //         this.descriptionText = striptags(this.description)
-    //     }
-    // }
+	// @BeforeInsert()
+	// @BeforeUpdate()
+	// setDescriptionText = (): void => {
+	//     if(this.description) {
+	//         this.descriptionText = striptags(this.description)
+	//     }
+	// }
 }
